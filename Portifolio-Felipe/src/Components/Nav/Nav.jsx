6093303ebd5d9fc88c69938fd007c0b46791d01f
Felipeaugustobezerra <58082993+Felipeaugustobezerra@users.noreply.github.com>
2024-12-16 19:26:30 +0000

    Add Aside and component Nav

---
+++ Portifolio-Felipe/src/Components/Nav/Nav.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useState } from "react";
+import "./nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineMenu } from "react-icons/ai";
+import { MdCardTravel } from "react-icons/md";
+import { BiMessageRounded } from "react-icons/bi";
+
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
+  return (
+    <nav className="nav">
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <AiOutlineMenu />
+      </a>
+      <a
+        href="#portifolio"
+        onClick={() => setActiveNav("#portifolio")}
+        className={activeNav === "#portifolio" ? "active" : ""}
+      >
+        <MdCardTravel />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <BiMessageRounded />
+      </a>
+    </nav>
+  );
+};
+
+export default Nav;
--- Portifolio-Felipe/src/main.jsx
+++ Portifolio-Felipe/src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "../src/assets/colors/color-1.css";
 import Home from "./pages/Home/Home";
+import Aside from "./pages/Aside/Aside";
+import Nav from "../src/Components/Nav/Nav";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Home />
+    <Aside />
+    <Nav />
   </StrictMode>
 );
